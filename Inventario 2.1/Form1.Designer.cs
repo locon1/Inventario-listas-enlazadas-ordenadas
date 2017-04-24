608cc7f510f77775f574c32b51e7fd18109fd210
José Ocon <locon1@ucol.mx>
2017-04-24 07:34:45 -0500

    Add files via upload

---
+++ Inventario 2.1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Inventario_2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdReporte = new System.Windows.Forms.Button();
+            this.cmdBuscar = new System.Windows.Forms.Button();
+            this.cmdEliminar = new System.Windows.Forms.Button();
+            this.cmdAgregar = new System.Windows.Forms.Button();
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.lblCosto = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdReporte
+            // 
+            this.cmdReporte.Location = new System.Drawing.Point(498, 327);
+            this.cmdReporte.Name = "cmdReporte";
+            this.cmdReporte.Size = new System.Drawing.Size(75, 23);
+            this.cmdReporte.TabIndex = 45;
+            this.cmdReporte.Text = "Reporte";
+            this.cmdReporte.UseVisualStyleBackColor = true;
+            this.cmdReporte.Click += new System.EventHandler(this.cmdReporte_Click);
+            // 
+            // cmdBuscar
+            // 
+            this.cmdBuscar.Location = new System.Drawing.Point(128, 231);
+            this.cmdBuscar.Name = "cmdBuscar";
+            this.cmdBuscar.Size = new System.Drawing.Size(75, 23);
+            this.cmdBuscar.TabIndex = 42;
+            this.cmdBuscar.Text = "Buscar";
+            this.cmdBuscar.UseVisualStyleBackColor = true;
+            this.cmdBuscar.Click += new System.EventHandler(this.cmdBuscar_Click);
+            // 
+            // cmdEliminar
+            // 
+            this.cmdEliminar.Location = new System.Drawing.Point(128, 277);
+            this.cmdEliminar.Name = "cmdEliminar";
+            this.cmdEliminar.Size = new System.Drawing.Size(75, 23);
+            this.cmdEliminar.TabIndex = 41;
+            this.cmdEliminar.Text = "Eliminar";
+            this.cmdEliminar.UseVisualStyleBackColor = true;
+            this.cmdEliminar.Click += new System.EventHandler(this.cmdEliminar_Click);
+            // 
+            // cmdAgregar
+            // 
+            this.cmdAgregar.Location = new System.Drawing.Point(22, 277);
+            this.cmdAgregar.Name = "cmdAgregar";
+            this.cmdAgregar.Size = new System.Drawing.Size(75, 23);
+            this.cmdAgregar.TabIndex = 40;
+            this.cmdAgregar.Text = "Agregar";
+            this.cmdAgregar.UseVisualStyleBackColor = true;
+            this.cmdAgregar.Click += new System.EventHandler(this.cmdAgregar_Click);
+            // 
+            // txtLista
+            // 
+            this.txtLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLista.Location = new System.Drawing.Point(220, 12);
+            this.txtLista.Multiline = true;
+            this.txtLista.Name = "txtLista";
+            this.txtLista.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLista.Size = new System.Drawing.Size(353, 306);
+            this.txtLista.TabIndex = 39;
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(19, 186);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(184, 20);
+            this.txtCosto.TabIndex = 38;
+            // 
+            // lblCosto
+            // 
+            this.lblCosto.AutoSize = true;
+            this.lblCosto.Location = new System.Drawing.Point(19, 170);
+            this.lblCosto.Name = "lblCosto";
+            this.lblCosto.Size = new System.Drawing.Size(37, 13);
+            this.lblCosto.TabIndex = 37;
+            this.lblCosto.Text = "Costo:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(19, 136);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(184, 20);
+            this.txtCantidad.TabIndex = 36;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(19, 120);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
+            this.lblCantidad.TabIndex = 35;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(19, 84);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(184, 20);
+            this.txtNombre.TabIndex = 34;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(19, 68);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 33;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(19, 35);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(184, 20);
+            this.txtCodigo.TabIndex = 32;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(19, 19);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 31;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 376);
+            this.Controls.Add(this.cmdReporte);
+            this.Controls.Add(this.cmdBuscar);
+            this.Controls.Add(this.cmdEliminar);
+            this.Controls.Add(this.cmdAgregar);
+            this.Controls.Add(this.txtLista);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.lblCosto);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "Form1";
+            this.Text = "Inventario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdReporte;
+        private System.Windows.Forms.Button cmdBuscar;
+        private System.Windows.Forms.Button cmdEliminar;
+        private System.Windows.Forms.Button cmdAgregar;
+        private System.Windows.Forms.TextBox txtLista;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Label lblCosto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblCodigo;
+    }
+}
+
---
+++ Inventario 2.1/Inventario.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario_2._1
+{
+    class Inventario
+    {
+        private Producto primero;
+        private int _cont;
+        public int cont { get { return _cont; } }
+
+        public Inventario()
+        {
+            primero = null;
+            _cont = 0;
+        }
+
+        public void Agregar(Producto nuevo)
+        {
+            if (primero == null) 
+            {
+                primero = nuevo;
+            }
+            else if (nuevo.codigo < primero.codigo) 
+            {
+                nuevo.siguiente = primero;
+            }
+            else 
+            {
+                Producto temp = primero;
+                while (temp.siguiente != null) 
+                {
+                    if (nuevo.codigo < temp.siguiente.codigo) 
+                    {
+                        Producto x = temp.siguiente;
+                        temp.siguiente = nuevo;
+                        temp.siguiente.siguiente = x;
+                        break;
+                    }
+                    temp = temp.siguiente;
+                }
+            }
+            _cont++;
+        }
+
+        public bool Eliminar(string name)
+        {
+            if (primero.nombre == name) {
+                primero = primero.siguiente;
+                return true;
+            }
+            else {
+                Producto temp = primero;
+                while (temp.siguiente.nombre != name && temp.siguiente != null) {
+                    temp = temp.siguiente;
+                }
+
+                if (temp.siguiente.nombre == name) {
+                    temp.siguiente = temp.siguiente.siguiente;
+                    _cont--;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        public Producto Buscar(string name)
+        {
+            Producto temp = primero;
+            while (temp.nombre != name && temp.siguiente != null) {
+                temp = temp.siguiente;
+            }
+
+            if (temp.nombre == name) {
+                return temp;
+            }
+            else {
+                return null;
+            }
+        }
+
+        public string Reporte()
+        {
+            string salida = "Número de productos en el inventario: " + _cont + Environment.NewLine + Environment.NewLine + "Inventario: " + Environment.NewLine + Environment.NewLine;
+            Producto temp = primero;
+            while (temp != null) {
+                salida += temp.ToString() + Environment.NewLine;
+                salida += "---------------------------------------------------------------------------" + Environment.NewLine;
+                temp = temp.siguiente;
+            }
+            return salida;
+        }
+    }
+}
